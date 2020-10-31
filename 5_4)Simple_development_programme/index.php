961c57246d7ef663fb889c3cbd96addae4956157
sandeepmchary <awssandeepchary@gmail.com>
2020-10-31 22:51:44 +0530

    added index.php

--- 5_4)Simple_development_programme/index.php
+++ 5_4)Simple_development_programme/index.php
@@ -2,7 +2,7 @@
 header("content-type: text");
 $host = "db";// The host name db from our docker-compose file
 $username = "root"; // We use the root password
-$pw = "my!!root!!passwd" // This the password we set in the docker-compose file
+$pw = "my!!root!!passwd"; // This the password we set in the docker-compose file
 $conn=new mysqli($host,$username,$pw);
 if($conn->connect_errno > 0){
     echo $db->connect_error;
